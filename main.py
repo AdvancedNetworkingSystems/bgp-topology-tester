1515cdd3f4b3d83c947375c0f0b4cde4cd4b2439
Leonardo Maccari <leonardo.maccari@unitn.it>
2019-07-31 12:22:20 +0200

    now incremental logs

--- main.py
+++ main.py
@@ -33,34 +33,37 @@ def gen_graph(x):
     return x, g, (end_time-start_time).total_seconds()
 
 
-def print_res(times):
+def print_res(times, runs):
     padding = 20
-    print("size".ljust(padding), "average time (s)".ljust(padding),
-          "std dev (s)".ljust(padding), "human readable".ljust(padding))
+    if not times:
+        print("size".ljust(padding), "average time (s)".ljust(padding),
+              "std dev (s)".ljust(padding), "human readable".ljust(padding))
+        return
     for x in times:
-        human_r = str(datetime.timedelta(seconds=sum(times[x])/len(times[x])))
-        print(str(x).ljust(padding),
-              str(round(statistics.mean(times[x]), 5)).ljust(padding),
-              str(round(statistics.stdev(times[x]), 5)).ljust(padding),
-              human_r.ljust(padding))
+        if len(times[x]) == runs and x not in printed_res:
+            human_r = str(datetime.timedelta(seconds=sum(times[x])/len(times[x])))
+            print(str(x).ljust(padding),
+                  str(round(statistics.mean(times[x]), 5)).ljust(padding),
+                  str(round(statistics.stdev(times[x]), 5)).ljust(padding),
+                  human_r.ljust(padding))
+            printed_res.add(x)
 
 
 args = parse_args()
 times = defaultdict(list)
 graphs = defaultdict(list)
+printed_res = set()
+
 
 arg_list = []
 for x in range(args.s, args.e+1, args.d):
     arg_list += [x]*args.r
 pool = Pool(args.p)
+print_res(times, args.r)
 for x, g, t in pool.imap_unordered(gen_graph, arg_list):
     times[x].append(t)
     graphs[x].append(g)
-
-print_res(times)
-
-if args.S:
-    for x in graphs:
-        for idx, g in enumerate(graphs[x]):
-            nx.write_graphml(g, "%s/internet-AS-graph-%d-%d.graphml" % (
-                args.S, x, idx))
+    print_res(times, args.r)
+    if args.S:
+        nx.write_graphml(g, "%s/internet-AS-graph-%d-%d.graphml" % (
+                    args.S, x, len(graphs[x])))
